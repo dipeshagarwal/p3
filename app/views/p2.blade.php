e8ac671405724f3b6cd6a80296eb056765918e9d
Agarwal Dipesh <dipeshagarwal2003@gmail.com>
2014-10-25 01:49:47 +0530

    Completed p3

--- app/views/p2.blade.php
+++ app/views/p2.blade.php
@@ -131,24 +131,24 @@ if ($words = file(app_path().'/database/p2/words.txt'))
 @section('content')
 
     <div class="container">
-<h2>xkcd Style Password Generator</h2>
-<h4>This Password Generator generates passwords consisting from common words. Such words are hard to guess (even by brute force), but easy to remember, making them interesting password choices.</h4>
-<h3>Fill Details</h3>
-<form method="GET" action="p2">
-<label name="countwords">Number of words</label>
-<input type="text" id="countwords" name="countwords" value="3"/><br />
-<label name="lengthpassword">Maximum Length of password</label>
-<input type="text" id="lengthpassword" name="lengthpassword" value="4"/><br />
-<label name="uppercase">Upper case First Letter?</label>
-<input type="checkbox" name="uppercase" value="checkbox" <?php echo ($uppercase) ? 'checked = "checked"' : ''; ?>/><br />
-<label name="symbol">Use a symbol?</label>
-<input type="checkbox" name="symbol" value="symbol" /><br />
-<label name="number">Include a number?</label>
-<input type="checkbox" name="number" value="number" /><br />
-<input type="submit" name="submit" value="Generate a new Password"/>
-</form>
-<br />
-<p>Your Password is:</p>
-<p><?php echo ($password) ? $password : ''; ?></p>
-</div>
+    <h2>xkcd Style Password Generator</h2>
+    <h4>This Password Generator generates passwords consisting from common words. Such words are hard to guess (even by brute force), but easy to remember, making them interesting password choices.</h4>
+    <h3>Fill Details</h3>
+    <form method="GET" action="p2">
+    <label name="countwords">Number of words</label>
+    <input type="text" id="countwords" name="countwords" value="3"/><br />
+    <label name="lengthpassword">Maximum Length of password</label>
+    <input type="text" id="lengthpassword" name="lengthpassword" value="4"/><br />
+    <label name="uppercase">Upper case First Letter?</label>
+    <input type="checkbox" name="uppercase" value="checkbox" <?php echo ($uppercase) ? 'checked = "checked"' : ''; ?>/><br />
+    <label name="symbol">Use a symbol?</label>
+    <input type="checkbox" name="symbol" value="symbol" /><br />
+    <label name="number">Include a number?</label>
+    <input type="checkbox" name="number" value="number" /><br />
+    <input type="submit" name="submit" value="Generate a new Password"/>
+    </form>
+    <br />
+    <p>Your Password is:</p>
+    <p><?php echo ($password) ? $password : ''; ?></p>
+    </div>
 @stop
